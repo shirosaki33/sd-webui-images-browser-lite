c2c5a516dabc3ec9bc89447b47e0dc4a1874ac93
shirosaki33 <95938549+shirosaki33@users.noreply.github.com>
2026-04-30 16:47:34 -0300

    Delete preload.py

--- preload.py
+++
@@ -1,8 +0,0 @@
-
-
-def preload(parser):
-    parser.add_argument(
-        "--image-browser-tmp-db",
-        action="store_true",
-        help="Copy database file to and from /tmp when transacting (workaround for filesystems sqlite does not support)"
-    )
